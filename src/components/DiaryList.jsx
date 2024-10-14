8cf868853e7974d5f6e24046b3f42ff8e468612b
NaNa923 <k5279kr@gmail.com>
2024-10-14 14:04:06 +0900

    css수정, vercel.json추가

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -6,11 +6,11 @@ import { useState } from "react";
 
 const DiaryList = ({ data }) => {
   const nav = useNavigate();
-  const [sortedType, setSotredType] = useState("lasted");
+  const [sortedType, setSortedType] = useState("lasted");
 
   // select로 sortedType 변경시 작동하는 함수
   const onChangeSortType = (e) => {
-    setSotredType(e.target.value);
+    setSortedType(e.target.value);
   };
 
   // sortedType 따라 데이터 정렬하는 함수
@@ -29,10 +29,22 @@ const DiaryList = ({ data }) => {
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select onChange={onChangeSortType}>
-          <option value="lasted">최신순</option>
-          <option value="oldsted">오래된순</option>
-        </select>
+        <div className="select_box">
+          <select onChange={onChangeSortType}>
+            <option value="lasted">최신순</option>
+            <option value="oldsted">오래된순</option>
+          </select>
+        </div>
+        <div className="select_box">
+          <select>
+            <option value="all">전체</option>
+            <option value="1">완전 좋음</option>
+            <option value="2">좋음</option>
+            <option value="3">그럭저럭</option>
+            <option value="4">나쁨</option>
+            <option value="5">끔찍함</option>
+          </select>
+        </div>
         <Button
           onClick={() => nav("/new")}
           text={"새 일기쓰기"}
